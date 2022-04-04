fb54d2811ab60842ef4231099e68bedea9c8ff1e
anastasia <anastasitkachenko@gmail.com>
2022-04-04 18:03:57 +0200

    pages:posts,admin,about,dynamic _id attached to posts

---
+++ pages/about/index.vue
@@ -0,0 +1,15 @@
+<template>
+   <div class="about-page">
+          <h1>This is my absolutely fantastic "About Me" Page!</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque excepturi eos pariatur cumque officia harum doloribus accusamus ipsum tempora velit!</p>
+       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque excepturi eos pariatur cumque officia harum doloribus accusamus ipsum tempora velit!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque excepturi eos pariatur cumque officia harum doloribus accusamus ipsum tempora velit!</p>
+   </div>
+</template>
+
+<style scoped>
+.about-page{
+   text-align:center;
+   padding:30px;
+}
+</style>
